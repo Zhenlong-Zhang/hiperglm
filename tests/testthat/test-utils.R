e0faae2e4f22ec3087a06af5b22e82477aa255db
Zhenlong Zhang <zzl010824@gmail.com>
2025-02-17 15:02:32 +0800

    code repetitions
    use are all close without redefining

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,9 +1,8 @@
-test_that("are_all_close works correctly", {
-  are_all_close <- function(a, b, rel_tol = 1e-5, abs_tol = 1e-8) {
-    diff <- abs(a - b)
-    return(all(diff < abs_tol | diff / (abs(b) + abs_tol) < rel_tol))
-  }
+# where to get the function needed for testing
+library(testthat)
+source("utils.R")
 
+test_that("are_all_close works correctly", {
   # test 1: value close，should be TRUE
   expect_true(are_all_close(1.000001, 1.000002, rel_tol = 1e-5))
 
